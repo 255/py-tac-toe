918d0af18525efab1e7cb2b363873ccbb4ac0f85
Sean Lindsay <seancl4@gmail.com>
2017-08-05 20:46:58 -0700

    Implement function to apply moves

--- py-tac-toe.py
+++ py-tac-toe.py
@@ -24,8 +24,18 @@ def isValidMove(board, move):
 	currentValue = getTile(board, tile)
 	return currentValue == tile
 
+def applyMove(board, tile, marker):
+	tile = tile - 1
+	row = tile // 3
+	col = tile % 3
+	board[row][col] = marker
+
 print("Welcome to Py-Tac-Toe!")
 printBoard(board)
 
-userInput = input("Your move [1-9]: ")
-print(isValidMove(board, userInput))
+userInput = input("You are X. Your move [1-9]? ")
+if isValidMove(board, userInput):
+	applyMove(board, int(userInput), "X")
+	printBoard(board)
+else:
+	print("Whoops! Your input was invalid, please type a legal move between 1 and 9")
