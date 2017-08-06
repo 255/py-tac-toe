d191803cdba41243753a5fdb37f5a5ab4c0fc11e
Sean Lindsay <seancl4@gmail.com>
2017-08-05 19:50:38 -0700

    Add basic validation function

--- py-tac-toe.py
+++ py-tac-toe.py
@@ -7,5 +7,18 @@ def printBoard(board):
 		if row != 2:
 			print(horizontalDivider)
 
+def getTile(board, tile):
+	tile = tile - 1
+	row = tile // 3
+	col = tile % 3
+	return board[row][col]
+
+def isValidMove(board, move):
+	currentValue = getTile(board, move)
+	return currentValue == move
+
 print("Welcome to Py-Tac-Toe!")
 printBoard(board)
+
+move = int(input("Your move [1-9]: "))
+print(isValidMove(board, move))
